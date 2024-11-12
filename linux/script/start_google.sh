48b7d06bd60bfe4fe00f96965f13af4210a0ec45
Batr <poplavkovsemen@gmail.com>
2024-11-12 18:00:01 +0800

    Work_18:00 12.11

--- linux/script/start_google.sh
+++ linux/script/start_google.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 ./opt/google/chrome/chrome --start-fullscreen
+#sudo nano /etc/systemd/system/start_google.service
+#sudo systemctl enable start_google.service
+ini
+[Unit]
+Description=Запуск Chrome
+[Service]
+ExecStart=/home/tv/script/google_start.sh
+Type=simple
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+
+
+#@reboot /home/tv/script/google_start.sh
+
+
+
+@reboot sleep 10 && /home/tv/script/google_start.sh >/dev/null 2>&1
