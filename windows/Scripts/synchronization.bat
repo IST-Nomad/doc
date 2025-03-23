67e884c47cab92436868c83405fe5cef28dcb3d1
Batr <istnomad@yandex.ru>
2025-03-23 18:00:02 +0800

    Work_18:00 23.03

--- windows/Scripts/synchronization.bat
+++ windows/Scripts/synchronization.bat
@@ -5,3 +5,7 @@ rem копируем
 robocopy F:\book D:\book *.pdf *.epub /E /Z /R:3 /W:1
 robocopy F:\Soft D:\Soft /E /Z /R:3 /W:1
 robocopy C:\Users\batr\Downloads D:\book *.pdf *.epub /MOVE /E /Z /R:3 /W:1
+
+
+/COPYALL
+robocopy L:\ O:\ /E /COPYALL /MIR /Z /R:3 /W:1
