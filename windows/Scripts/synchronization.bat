66eedc522dd800fbacf9517c2456175b1376fd08
Batr <istnomad@yandex.ru>
2025-03-27 18:00:02 +0800

    Work_18:00 27.03

--- windows/Scripts/synchronization.bat
+++ windows/Scripts/synchronization.bat
@@ -7,5 +7,8 @@ robocopy F:\Soft D:\Soft /E /Z /R:3 /W:1
 robocopy C:\Users\batr\Downloads D:\book *.pdf *.epub /MOVE /E /Z /R:3 /W:1
 
 
-/COPYALL
-robocopy L:\ O:\ /E /COPYALL /MIR /Z /R:3 /W:1
+rem Копирование со старого диска Т на новый
+robocopy \\10.129.135.205\EXCHANGE \\qnap246\disk_t /E /R:1 /W:0 
+
+rem Копирование вебинаров со старого диска Q на новый диск Т
+robocopy \\qnap247\!r!\!Вебинары! \\qnap246\disk_t\вебинары /E /R:1 /W:0 
